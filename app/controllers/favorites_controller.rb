98ff0cb41dcf01eec7c58f06028aafa9c490218a
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-06-29 00:03:08 +0000

    [Finish]いいね実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,30 +1,17 @@
 class FavoritesController < ApplicationController
+
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-
-    path = Rails.application.routes.recognize_path(request.referer)
-    if path[:action] == "books#index"
-
-    redirect_to book_path(book)
-    else
-    redirect_to book_path(book)
-    end
-
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-
-    if current_page?(books_path)
-
-    redirect_to book_path(book)
-    else
-    redirect_to book_path(book)
-    end
-
+    redirect_back(fallback_location: root_path)
+   
   end
 end
