e3a189077bf0db24f4d2ad71b8a91b5840ef8441
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-07-05 09:15:26 +0000

    [Add]コメント機能非同期化

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,19 +2,26 @@ class PostCommentsController < ApplicationController
 
 # コメント作成
   def create
-  book = Book.find(params[:book_id])
+  @book = Book.find(params[:book_id])
   comment = PostComment.new(post_comment_params)
   comment.user_id = current_user.id
-  comment.book_id = book.id
+  comment.book_id = @book.id
   comment.save
-  # redirect_to book_path(book)
+  @post_comment = PostComment.new
+
+  # redirect_to book_path(book)→非同期
   end
 
 # コメント削除
   def destroy
-    PostComment.find(params[:id]).destroy
-    # redirect_to book_path(params[:book_id])
+  # PostComment.find(params[:id]).destroy
+  # redirect_to book_path(params[:book_id])→非同期
+  @book=Book.find(params[:book_id])
+  @post_comment=PostComment.find_by(id: params[:id], book_id: params[:book_id])
+  @post_comment.destroy
+  @post_comment = PostComment.new
   end
+
 
   private
 
